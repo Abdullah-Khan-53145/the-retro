03a9a14db81288c18fb01cb0f8f8537a613ac462
Abdullah <abdullah.khan53145@gmail.com>
2022-11-19 17:54:20 +0500

    test 1 all-product-page

--- src/Components/AllProducts.js
+++ src/Components/AllProducts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "../Styles/allproducts.css";
 
 function AllProducts() {
@@ -25,67 +26,94 @@ function AllProducts() {
     brands: ["Addidas", "Nike", "Under Haumour", "Puma"],
   };
   const shoe = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const [position, setPosition] = useState({});
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        window.scrollY >=
+        document.querySelector(".all_products_filter").offsetTop
+      ) {
+        setPosition({ position: "fixed", top: 0 });
+      } else if (
+        window.scrollY <
+        document.querySelector(".all_products_filter").offsetTop
+      ) {
+        setPosition({ position: "absolute", top: 0 });
+      }
+      if (
+        window.scrollY >=
+        document.querySelector(".all_products_filter").offsetTop +
+          document.querySelector(".all_products_filter").offsetHeight -
+          window.innerHeight
+      ) {
+        setPosition({ position: "absolute", bottom: 0 });
+      }
+    });
+  }, []);
   return (
     <div className="all_products_main">
       <div className="all_products_main_child">
         <div className="all_products_filter">
-          <div className="all_products_filter_heading">
-            <h2>Filters</h2>
-            <div className="all_products_filter_heading_image">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
-                />
-              </svg>
+          <div className="all_products_filter_child" style={position}>
+            <div className="all_products_filter_heading">
+              <h2>Filters</h2>
+              <div className="all_products_filter_heading_image">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+                  />
+                </svg>
+              </div>
             </div>
-          </div>
-          <div className="all_products_filter_price all_products_filter_section">
-            <h3>Price</h3>
-            <div className="all_products_filter_price_range">
-              <input type="number" placeholder="Min" name="" id="" />
-              <input type="number" placeholder="Max" name="" id="" />
+            <div className="all_products_filter_price all_products_filter_section">
+              <h3>Price</h3>
+              <div className="all_products_filter_price_range">
+                <input type="number" placeholder="Min" name="" id="" />
+                <input type="number" placeholder="Max" name="" id="" />
+              </div>
             </div>
-          </div>
-          <div className="all_products_filter_sizes all_products_filter_section">
-            <h3>Size</h3>
-            <div className="all_products_filter_size_range">
-              {filterObj.sizes.map((size, index) => (
-                <div className="dis_con size" key={index}>
-                  {size}
-                </div>
-              ))}
+            <div className="all_products_filter_sizes all_products_filter_section">
+              <h3>Size</h3>
+              <div className="all_products_filter_size_range">
+                {filterObj.sizes.map((size, index) => (
+                  <div className="dis_con size" key={index}>
+                    {size}
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-          <div className="all_products_filter_colors all_products_filter_section">
-            <h3>Colors</h3>
-            <div className="all_products_filter_color_range">
-              {filterObj.colors.map((color, index) => (
-                <div
-                  className="dis_con color"
-                  key={index}
-                  style={{ backgroundColor: color }}
-                ></div>
-              ))}
+            <div className="all_products_filter_colors all_products_filter_section">
+              <h3>Colors</h3>
+              <div className="all_products_filter_color_range">
+                {filterObj.colors.map((color, index) => (
+                  <div
+                    className="dis_con color"
+                    key={index}
+                    style={{ backgroundColor: color }}
+                  ></div>
+                ))}
+              </div>
             </div>
-          </div>
-          <div className="all_products_filter_brands all_products_filter_section">
-            <h3>Brands</h3>
-            <div className="all_products_filter_brand_range">
-              {filterObj.brands.map((brand, index) => (
-                <div className="brand" key={index}>
-                  <p>{brand}</p>
-                  <input type="checkbox" name="" id="" />
-                </div>
-              ))}
+            <div className="all_products_filter_brands all_products_filter_section">
+              <h3>Brands</h3>
+              <div className="all_products_filter_brand_range">
+                {filterObj.brands.map((brand, index) => (
+                  <div className="brand" key={index}>
+                    <p>{brand}</p>
+                    <input type="checkbox" name="" id="" />
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
